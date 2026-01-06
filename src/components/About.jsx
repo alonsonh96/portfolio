67c4203b5c35f4a98b344f2cae86509f1beacd3d
Alonso <nichohuaman16@gmail.com>
2026-01-06 02:25:56 -0500

    feat(about): add animated section title with decorative lines and responsive layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,38 @@ import Section from './Section'
 
 const About = () => {
   return (
-    <Section id='sobre mí'>
-      <motion.div
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: false, amount: 0.3 }}
-        transition={{ duration: 0.7, delay: 0.2 }}
-        className='max-w-7xl mx-auto flex flex-col-reverse md:flex-row gap-16 items-center'
-      >
-        <div className='flex-1 text-center '>
-          <h2 className='text-lg uppercase tracking-widest text-gray-400 mb-6'>
+    <Section id='sobre mí' className=''>
+      <div className='max-w-7xl mx-auto flex flex-col-reverse md:flex-row gap-16 md:gap-8 items-center'>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: false, amount: 0.3 }}
+          transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1], delay: 0.2 }}
+          className='flex-1 md:text-left'
+        >
+          <h2 className='text-2xl md:text-5xl  font-bold tracking-widest text-white mb-2'>
             Sobre mí
           </h2>
-
-          <p className='text-lg text-white mb-6 leading-relaxed text-justify'>
+          {/* Líneas decorativas */}
+          <div className='space-y-2 mb-5'>
+            <motion.span
+              className='block h-0.5 w-20 md:w-30 bg-blue-400'
+              initial={{ scaleX: 0 }}
+              whileInView={{ scaleX: 1 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.6, delay: 0.2, ease: 'easeOut' }}
+              style={{ originX: 0 }}
+            />
+            <motion.span
+              className='block h-0.5 w-12 md:w-16 bg-blue-400/70'
+              initial={{ scaleX: 0 }}
+              whileInView={{ scaleX: 1 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.6, delay: 0.35, ease: 'easeOut' }}
+              style={{ originX: 0 }}
+            />
+          </div>
+          <p className='text-sm md:text-lg text-white mb-6 leading-relaxed text-justify'>
             Hola, soy Alonso, Ingeniero de Sistemas y desarrollador web con
             enfoque en Frontend y Backend. Me especializo en crear aplicaciones
             modernas, escalables y bien estructuradas, combinando buenas
@@ -25,30 +43,28 @@ const About = () => {
             Disfruto transformar ideas en soluciones digitales funcionales y
             mantenibles.
           </p>
-
-          <p className='text-lg text-gray-300 leading-relaxed text-justify'>
+          <p className='text-sm md:text-lg text-gray-300 leading-relaxed text-justify'>
             Más allá del código, mantengo una curiosidad constante por nuevas
             tecnologías, patrones de diseño y optimización de rendimiento. Creo
             firmemente que el aprendizaje continuo es clave para construir
             productos digitales sólidos y bien pensados.
           </p>
-        </div>
+        </motion.div>
         <motion.div
-          initial={{ opacity: 0, scale: 0.95 }}
-          whileInView={{ opacity: 1, scale: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.8, delay: 0.2 }}
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: false, amount: 0.3 }}
+          transition={{ duration: 0.8, delay: 0.2, ease: [0.22, 1, 0.36, 1] }}
           className='flex-1 relative flex justify-center md:display-block'
         >
           <div className='absolute inset-0 bg-purple-500/15 blur-3xl rounded-full animate-pulse' />
           <img
             src={'/src/assets/about.webp'}
             alt='Foto de Alonso'
-            // className='relative z-10 rounded-full shadow-2xl p-10'
             className='relative z-10 w-56 sm:w-64 md:w-72 lg:w-80 aspect-square object-cover rounded-full shadow-2xl'
           />
         </motion.div>
-      </motion.div>
+      </div>
     </Section>
   )
 }
