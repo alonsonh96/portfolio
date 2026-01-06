37831d806fe4502f0e5ee69b41956a95f3d0a0db
Alonso <nichohuaman16@gmail.com>
2026-01-05 23:55:42 -0500

    feat(about): polished about section with visuals and cleaner text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,36 +6,48 @@ const About = () => {
   return (
     <Section id='sobre mí'>
       <motion.div
-        initial={{ opacity: 0, y: 30}}
+        initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: false, amount: 0.3 }}
         transition={{ duration: 0.7, delay: 0.2 }}
-        className='max-w-4xl'
+        className='max-w-7xl mx-auto flex flex-col-reverse md:flex-row gap-16 items-center'
       >
-        <h2 className='text-sm uppercase tracking-widest text-gray-400 mb-6'>
-          Sobre mí
-        </h2>
+        <div className='flex-1 text-center '>
+          <h2 className='text-lg uppercase tracking-widest text-gray-400 mb-6'>
+            Sobre mí
+          </h2>
 
-        <p className='text-lg text-white mb-6 leading-relaxed'>
-          Hola, soy Alonso, Ingeniero de Sistemas y desarrollador web con
-          enfoque en Frontend y Backend. Me apasiona construir aplicaciones
-          modernas, escalables y bien estructuradas, combinando buenas prácticas
-          de desarrollo con una experiencia de usuario cuidada. Mi trabajo
-          conecta tecnologías web actuales con arquitecturas eficientes, desde
-          el desarrollo de interfaces responsivas y dinámicas hasta la
-          implementación de lógica backend robusta y APIs bien diseñadas.
-          Disfruto transformar ideas en soluciones digitales funcionales y
-          mantenibles.
-        </p>
+          <p className='text-lg text-white mb-6 leading-relaxed text-justify'>
+            Hola, soy Alonso, Ingeniero de Sistemas y desarrollador web con
+            enfoque en Frontend y Backend. Me especializo en crear aplicaciones
+            modernas, escalables y bien estructuradas, combinando buenas
+            prácticas de desarrollo con una experiencia de usuario cuidada.
+            Disfruto transformar ideas en soluciones digitales funcionales y
+            mantenibles.
+          </p>
 
-        <p className='text-lg text-gray-400 leading-relaxed'>
-          Más allá del código, mantengo una curiosidad constante por nuevas
-          tecnologías, patrones de diseño y mejoras en rendimiento. En un
-          entorno tecnológico que evoluciona rápidamente, creo firmemente que
-          aprender de forma continua no es una opción, sino una necesidad. Si te
-          interesa crear productos digitales sólidos y bien pensados, estaremos
-          hablando el mismo idioma
-        </p>
+          <p className='text-lg text-gray-300 leading-relaxed text-justify'>
+            Más allá del código, mantengo una curiosidad constante por nuevas
+            tecnologías, patrones de diseño y optimización de rendimiento. Creo
+            firmemente que el aprendizaje continuo es clave para construir
+            productos digitales sólidos y bien pensados.
+          </p>
+        </div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.95 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className='flex-1 relative flex justify-center md:display-block'
+        >
+          <div className='absolute inset-0 bg-purple-500/15 blur-3xl rounded-full animate-pulse' />
+          <img
+            src={'/src/assets/about.webp'}
+            alt='Foto de Alonso'
+            // className='relative z-10 rounded-full shadow-2xl p-10'
+            className='relative z-10 w-56 sm:w-64 md:w-72 lg:w-80 aspect-square object-cover rounded-full shadow-2xl'
+          />
+        </motion.div>
       </motion.div>
     </Section>
   )
